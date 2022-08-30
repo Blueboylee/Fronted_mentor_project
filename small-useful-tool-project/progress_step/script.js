cbd4cec96594a7b39a51d8ba9d898f0b76adb8ad
Blueboy <asukasaitolee@gmail.com>
2022-08-30 20:28:58 +0800

    final work

--- small-useful-tool-project/progress_step/script.js
+++ small-useful-tool-project/progress_step/script.js
@@ -1,7 +1,7 @@
-const prev = document.querySelector('#prev');
-const next = document.querySelector('#next');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle')
-const pg = document.querySelector('#progress');
+const pg = document.getElementById('progress');
 
 
 let origin = 1;
@@ -10,6 +10,7 @@ next.addEventListener('click', ()=>{
     if(origin > circles.length){
         origin = circles.length;
     }
+    update();
 
 })
 
@@ -21,7 +22,33 @@ prev.addEventListener('click', ()=>{
         origin = 1;
     }
 
+    update();
+
 })
 
 
+function update(){
+    circles.forEach((circle, idx) => {
+        if(origin > idx){
+            circle.classList.add('active');
+        }else{
+            circle.classList.remove('active');
+        }
+    })
+    const actives = document.querySelectorAll('.active');
 
+    pg.style.width = (actives.length-1) / (circles.length-1) * 100 + '%'
+    
+    
+    if(origin === 1){
+        prev.disabled = true;
+    }else if(origin === circles.length){
+        next.disabled = true;
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+
+
+
+}
