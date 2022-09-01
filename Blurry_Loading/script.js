b102199eceab39b712947938a3829e044cce3494
Blueboy <asukasaitolee@gmail.com>
2022-09-01 23:46:13 +0800

    final work

--- Blurry_Loading/script.js
+++ Blurry_Loading/script.js
@@ -1,18 +1,22 @@
 const bg = document.querySelector('.bg');
-const text1 = document.querySelector('.text');
+const text = document.querySelector('.text');
 
 
 let num = 0;
-let int = setInterval(blur, 10);
+let int = setInterval(blur, 30);
+
 function blur(){
     num++;
     if(num > 99){
         clearInterval(int);
     }
-    text1.innerText = `${num}%`
-    text1.style.opacity = scale(num,0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(num, 0, 100, 30, 0)}px)`
+
+    text.innerText = `${num}%`
+    text.style.opacity = scale(num, 0, 100, 1,0);
+    bg.style.filter = `blur(${scale(num,0,100,30,0)}px)`
+    
 }
+
 
 function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
